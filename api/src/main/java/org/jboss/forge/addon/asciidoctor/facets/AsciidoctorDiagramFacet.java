0ba2bda3fbd75ad063f85634423c88798bc4358b
Maxime Gréau <greaumaxime@gmail.com>
2014-09-21 22:57:48 +0200

    update to asciidoctor-diagram 1.2.1

--- api/src/main/java/org/jboss/forge/addon/asciidoctor/facets/AsciidoctorDiagramFacet.java
+++ api/src/main/java/org/jboss/forge/addon/asciidoctor/facets/AsciidoctorDiagramFacet.java
@@ -7,7 +7,7 @@ import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
 public interface AsciidoctorDiagramFacet extends AsciidoctorGemFacet
 {
    public static final Dependency ASCIIDOCTOR_DIAGRAM =
-            DependencyBuilder.create().setGroupId("rubygems").setArtifactId("asciidoctor-diagram").setVersion("1.2.0")
+            DependencyBuilder.create().setGroupId("rubygems").setArtifactId("asciidoctor-diagram").setVersion("1.2.1")
                      .setScopeType("provided").setPackaging("gem")
                      .addExclusion(CoordinateBuilder.create().setGroupId("rubygems").setArtifactId("asciidoctor"));
    
